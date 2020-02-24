9bd6ea87868a42c923248489d093d039f702aa63
Dzmitry Maridau <vinmaster@tut.by>
2020-02-25 01:22:37 +0300

    read markdown page date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,53 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default ({ data }) => {
+  //console.log(data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>DMA thougths </h1>
+        <h2>{data.allMarkdownRemark.totalCount} records</h2>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h4>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </h4>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            title
+          }
+          html
+          excerpt
+        }
+      }
+      totalCount
+    }
+  }
+`
