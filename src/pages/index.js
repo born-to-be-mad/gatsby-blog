f8762386e6249bccd59b852b85f43857c63ebc84
Dzmitry Maridau <vinmaster@tut.by>
2020-03-01 01:17:16 +0300

    Rework index js to styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,60 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Title = styled.h1`
+  display: inline-block;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  &:hover {
+    color: #1dcaff;
+  }
+`
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
+
 export default ({ data }) => {
-  //console.log(data)
   return (
     <Layout>
-      <SEO title="Home" />
       <div>
-        <h1>DMA thougths </h1>
-        <h2>{data.allMarkdownRemark.totalCount} records</h2>
+        <Title>DMA Thoughts</Title>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h4>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </h4>
-            <p>{node.excerpt}</p>
-          </div>
+          <BlogBody key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.frontmatter.description || node.excerpt}</p>
+          </BlogBody>
         ))}
       </div>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC } ) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
           frontmatter {
-            date
+            date(formatString: "DD MMMM, YYYY")
             title
           }
           html
